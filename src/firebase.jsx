4dfb5c502dfa3c98f1d3e65e3bba00a23ab9c8d2
vishmithSuranjaya <suranjaya0327@gmail.com>
2025-07-22 07:24:52 -0700

    change fav icon

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyhRcjRGp8CZs7ceenMLhFazlyEfxcJTM",
-  authDomain: "portfolio-vs-eecf7.firebaseapp.com",
-  projectId: "portfolio-vs-eecf7",
-  storageBucket: "portfolio-vs-eecf7.firebasestorage.app",
-  messagingSenderId: "804995397742",
-  appId: "1:804995397742:web:7a0c7c26610e280bc74e7f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
